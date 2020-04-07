6bc4063c64ab39eb9de9eb8d8373f7576ca9d617
Mahmoud Dardir <eng.mahmoud.dardir@gmail.com>
2020-04-07 07:51:24 +0200

    change to ES6 style

--- fileUploader.js
+++ fileUploader.js
@@ -4,10 +4,10 @@ exports.uploadFile =  (filePath, metadataObj) => {
   try {
     const fileContent = fs.readFileSync(filePath);
     if (fileContent.length === 0) {
-      console.log("Empty file in path = " + filePath);
+      console.log(`Empty file in path = ${filePath}`);
       return 'Empty File';
     }
-    console.log("read file Successfuly in path = " + filePath+ " with size = "+fileContent.length);
+    console.log(`read file Successfuly in path = ${filePath} with size = "${fileContent.length}`);
     return "Ok";
   } catch (err) {
     return err.message;
