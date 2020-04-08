38e36f1060b60ca40444ddf4eb84346c5145003b
Mahmoud Dardir <eng.mahmoud.dardir@gmail.com>
2020-04-08 17:39:32 +0200

    Add CORS response header

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const fu = require('./fileUploader');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/upload', function (req, res) {
   const metadataObj = req.body.metadataObj;
