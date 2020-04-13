8d2048a13b63f727edf46fba7cabdb3f5c03ab8b
Mahmoud Dardir <eng.mahmoud.dardir@gmail.com>
2020-04-13 19:27:06 +0200

    Implenting uploading document to Alfresco

--- alfresco.js
+++ alfresco.js
@@ -1,15 +1,70 @@
-exports.uploadFileToFolder = (file,metadataObj)=> {
+exports.uploadFileToFolder = (fileName, filePath, metadataObj) => {
     const yaml = require('js-yaml');
     const fs = require('fs');
+    const axios = require('axios');
+    const FormData = require('form-data');
     try {
         let configFile = fs.readFileSync('./alfresco.yaml', 'utf8');
         let config = yaml.safeLoad(configFile);
         console.log("Alfresco Configuration : ")
         console.log(config);
-        return "Ok";
+
+        //sending post request to Alfresco
+
+        const options = {
+            "headers": {
+                "content-type": "multipart/form-data; boundary=---011000010111000001101001",
+                "authorization": `${config.basicAuthorizationKey}`
+            }
+        };
+         
+        const form = new FormData();
+        form.append('name', fileName);
+        form.append('nodeType', config.alfrescoNodeType);
+        form.append('filedata', fs.createReadStream(filePath));
+        form.append('dc:Barcode', metadataObj.barcode);
+        form.append('dc:EDate', formatDate(metadataObj.delegationDate));
+        form.append('dc:Embassy', metadataObj.counsulate);
+        form.append('dc:EmployeeName', metadataObj.employeeName);
+        form.append('dc:EmployeeNo', metadataObj.employeeNumber);
+        form.append('dc:POADate', formatDate(metadataObj.transactionDate));
+        form.append('dc:POAFrom', metadataObj.delegator);
+        form.append('dc:POAFromPass', metadataObj.delegatorPassport);
+        form.append('dc:POAKeyword', metadataObj.keySearch);
+        form.append('dc:POANumber', metadataObj.delegationNumber);
+        form.append('dc:POASubject', metadataObj.delegationSubject);
+        form.append('dc:POATo', metadataObj.delegatedTo);
+        form.append('dc:POAToPass', metadataObj.delegatedToPassport);
+        form.append('dc:POAType', metadataObj.delegationType);
+
+        const url = `http://${config.host}:${config.port}${config.createDocumentURL}/${config.folderID}/children/`;
+        console.log(`calling URL : ${url}`);
+
+        axios.post(url, form, options)
+        .then(({ data }) => {
+            console.log(data);
+            return "Ok";
+        }, (error) => {
+            console.log(error.message);
+            return error.message;
+        })
     } catch (e) {
         console.log(e.message);
         return e.message;
     }
-    
+
 };
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
--- fileUploader.js
+++ fileUploader.js
@@ -1,15 +1,21 @@
 exports.uploadFile =  (filePath, metadataObj) => {
   const alfresco = require('./alfresco');
-  console.log(`Starting to read file in path = ${filePath}`);
-  const fs = require('fs');
+  console.log(`Starting to upload file in path = ${filePath}`);
+  //const fs = require('fs');
   try {
-    const fileContent = fs.readFileSync(filePath);
+    /*const fileContent = fs.readFileSync(filePath);
     if (fileContent.length === 0) {
       console.log(`Empty file in path = ${filePath}`);
       return 'Empty File';
+    }*/
+    //console.log(`read file Successfuly in path = ${filePath} with size = "${fileContent.length}`);
+    let pathArr = filePath.split("/");
+    if(pathArr.length === 0){
+      pathArr = filePath.split("\\");
     }
-    console.log(`read file Successfuly in path = ${filePath} with size = "${fileContent.length}`);
-    return alfresco.uploadFileToFolder(fileContent,metadataObj);
+    const fileName = pathArr[pathArr.length-1];
+    console.log(`file name to be sent is ${fileName}`);
+    return alfresco.uploadFileToFolder(fileName,filePath,metadataObj);
   } catch (err) {
     return err.message;
   }
