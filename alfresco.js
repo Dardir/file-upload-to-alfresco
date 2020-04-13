aadb73726a0c843e37e0d779008dbb08b4307c9a
Mahmoud Dardir <eng.mahmoud.dardir@gmail.com>
2020-04-13 15:34:38 +0200

    Reading Alfresco config from yaml file

--- alfresco.js
+++ alfresco.js
@@ -1,3 +1,15 @@
 exports.uploadFileToFolder = (file,metadataObj)=> {
-    return "Ok";
+    const yaml = require('js-yaml');
+    const fs = require('fs');
+    try {
+        let configFile = fs.readFileSync('./alfresco.yaml', 'utf8');
+        let config = yaml.safeLoad(configFile);
+        console.log("Alfresco Configuration : ")
+        console.log(config);
+        return "Ok";
+    } catch (e) {
+        console.log(e.message);
+        return e.message;
+    }
+    
 };
